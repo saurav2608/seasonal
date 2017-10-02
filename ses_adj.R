c09e921195110efe5e4607acc8c67a80abd0e342
Saurav Chakravorty <saurav.chakravorty@BRILLIO.COM>
2017-10-02 23:56:51 +0530

    Final analysis

--- ses_adj.R
+++ ses_adj.R
@@ -1,18 +1,37 @@
 library(tidyverse)
 library(seasonal)
 library(lubridate)
-car_sales <- read_csv("data/sales.csv")
-car_sales$Month <- lubridate::mdy(car_sales$Month)
-car_sales_ts <- ts(car_sales$Sales, 
-                   start = c(year(car_sales$Month[1]),
-                             month(car_sales$Month[1])), 
+library(forecast)
+library(ggthemes)
+theme_set(theme_economist_white())
+#India IIP data from below link (no API support)
+#https://data.gov.in/catalog/nic-2-digit-level-and-sectoral-monthly-indices-all-india-index-industrial-production
+
+# code 32 is white goods http://mospi.nic.in/sites/default/files/main_menu/national_industrial_classification/nic_2004_struc_2digit.pdf
+
+iip <- read_csv("data/india_iip_05_13.csv")
+month_M <- stringr::str_sub(iip$Month, 1, 3)
+year_y <- stringr::str_sub(iip$Month, 6, -1)
+
+iip$date <- lubridate::mdy(paste(month_M, "01", year_y, sep = "-"))
+
+prod_ts <- ts(iip$`NIC 32`, 
+                   start = c(year(iip$date[1]),
+                             month(iip$date[1])), 
                    frequency = 12)
+g <- ggplot(iip) +
+  geom_line(aes(y = `NIC 32`, x = date)) +
+  ggtitle("IIP Date - White Goods Production Index") +
+  ylab("White Good Prod Index")
+ggsave("output/ts.png", g, device = "png")
 
-diwali_reg <- genhol(diwali, start = -29, end = 0, frequency = 12, center = "calendar")
+diwali_reg <- genhol(diwali, start = -60, end = 0, frequency = 12, center = "calendar")
 
-m <- seas(car_sales_ts, 
-          xreg = diwali_reg, 
-          regression.aictest = NULL)
+m <- seas(prod_ts, 
+          xreg  = diwali_reg, 
+          regression.aictest = NULL,
+          regression.usertype = "holiday",
+          forecast.maxlead = 12)
 summary(m)
 plot(m, trend = T)
 monthplot(m)
@@ -20,6 +39,16 @@ pacf(resid(m))
 spectrum(diff(resid(m)))
 plot(density(resid(m)))
 qqnorm(resid(m))
-view(m)
-m$list
-plot(series(m, "series.adjoriginal"))
+
+output_df <- data.frame(date = iip$date, orig = as.numeric(original(m)), 
+                        final = as.numeric(final(m)), 
+                        trend = as.numeric(m$data[, "trend"]))
+g1 <- ggplot(output_df, aes(x = date)) +
+  geom_line(aes(y = orig, color = "blue")) +
+  geom_line(aes(y = final, color = "red")) +
+  geom_line(aes(y = trend, color = "gray"), linetype = 2) +
+  ggtitle("Original and Seasonally Adjusted Series - White Goods") +
+  theme(legend.position = "none") +
+  ylab("Series Values")
+g1  
+ggsave("output/ts1.png", g1, device = "png")
